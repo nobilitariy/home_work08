4ea4df87b0538918aaf4ea01a0af3f188b5792ad
artem_kopshik <artemktk@gmail.com>
2023-07-17 16:00:47 +0300

    add

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите натуральное числo:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Number(int number){
+    Console.Write($"{number} ");
+    if(number > 1) Number(number-1);      
+}
+Number(number);
+Console.WriteLine();
---
+++ task7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите два натуральных числа(первое число больше второго): ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int number1, int number2){
+    if(number1 == number2) return number1; { 
+    return number1 + SumNum(number1+1, number2);
+} 
+}
+
+int sum = SumNum(number1, number2);
+Console.WriteLine(sum);
