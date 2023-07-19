09d863086bd5acbee32ce41f97287befc1eb9b67
artem_kopshik <artemktk@gmail.com>
2023-07-19 14:06:32 +0300

    все

--- task5/Program.cs
+++ task5/Program.cs
@@ -6,22 +6,6 @@
     return array;
 }
 
-int GetRand(){
-    Random rand = new Random();
-    int number = rand.Next(1,11);
-    return number;
-}
-
-int [,] GetArray(int [] array){
-    int [,] arrayNew = new int [array[0], array[1]];
-    for(int i = 0; i < array[0]; i++){
-        for(int j = 0; j < array[1]; j++){
-            arrayNew[i, j] = GetRand();
-        }
-    }
-    return arrayNew;
-}
-
 void PrintArray(int [,] array){
     int row = array.GetLength(0);
     int col = array.GetLength(1);
@@ -32,3 +16,39 @@ void PrintArray(int [,] array){
         Console.WriteLine();
     }
 }
+
+int [,] Spiral(int [] array){
+    int [,] mass = new int [array[0], array[1]];
+    int value = 0;
+    int lin = 0;
+    int linFin = array[0]-1;
+    int col = 0;
+    int colFin = array[1]-1;
+    while(value < (array[0]*array[1])){
+        for(int i = col; i <= colFin; i++){
+            mass[lin, i] = value;
+            value++;
+        }
+        lin++;
+        for(int i = lin; i <= linFin; i++){
+            mass[i, colFin] = value;
+            value++;
+        }
+        colFin--;
+        for(int i = colFin; i >= col; i--){
+            mass[linFin, i] = value;
+            value++;
+        }
+        linFin--;
+        for(int i = linFin; i >= lin; i-- ){
+            mass[i, col] = value;
+            value++;
+        }
+        col++;
+    }
+    return mass;
+}
+
+int [] diap = GetArrayDiap();
+int [,] mass = Spiral(diap);
+PrintArray(mass);
---
+++ task8/Program.cs
@@ -0,0 +1,32 @@
+﻿int GetNumber(int number1, int number2){
+    if(number1 == 0) return number2+1;
+    else if(number2 == 0) return GetNumber(number1-1, 1);
+    else return GetNumber(number1-1, GetNumber(number1, number2-1));
+}
+
+Console.Write("Введите два неотрицательных числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int w = GetNumber(number1, number2);
+Console.WriteLine(w);
+
+/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+/*Console.Clear(); 
+
+int akkerman(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if (n == 0) return akkerman(m - 1, 1);
+        else return akkerman(m - 1, akkerman(m, n - 1));
+    }
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Функция Аккермана равна {akkerman(m, n)}\n ");
+*/
